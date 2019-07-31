e1109dea6b014997e18dae18b8f0538dc7b9a83b
Marcelo Adamatti <adamatti@gmail.com>
2019-07-30 23:07:22 -0300

    run db auth

--- src/db.rs
+++ src/db.rs
@@ -9,6 +9,7 @@ use mongodb::coll::Collection;
 
 use serde::Serialize;
 use bson::ordered::OrderedDocument;
+use std::env;
 
 const COLLECTION_NAME: &'static str = "tiddlers";
 
@@ -87,7 +88,18 @@ fn to_document<T: ?Sized>(obj:&T) -> Result<Document,Bson>
 
 pub fn connect() -> Database {
     let mongo_uri = get_env_var_or_default("MONGODB_URI","mongodb://localhost:27017");
-    let client = Client::with_uri(mongo_uri.as_str()).expect("Failed to initialize mongo client.");
+    let client:Client = Client::with_uri(mongo_uri.as_str()).expect("Failed to initialize mongo client.");
     let db_name = get_env_var_or_default("MONGODB_DATABASE","wiki_mongo");
-    return client.db(db_name.as_str());
+    let database: Database = client.db(db_name.as_str());
+
+    match [env::var("MONGODB_USER"), env::var("MONGODB_PASS")]{
+        [Ok(user),Ok(pass)] => {
+            database.auth(user.as_str(),pass.as_str()).expect("Error doing mongo auth");
+
+        },
+        [_,_] => {}
+    }
+
+
+    return database;
 }
